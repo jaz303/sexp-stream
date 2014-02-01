4a42ef9a514e6f266d74165d4338c357affef02e
Jason Frame <jason@onehackoranother.com>
2014-02-01 20:17:24 +0000

    first pass

---
+++ index.js
@@ -0,0 +1,63 @@
+var tokenize = require('sexp-tokenizer');
+
+var S_OUT		= 1,
+	S_IN_MAIN 	= 2,
+	S_IN_EXP 	= 3,
+	S_END 		= 4;
+
+module.exports = sexp;
+
+function Parser() {
+	this._state = S_OUT;
+}
+
+Parser.prototype._transform = function(chunk, encoding, done) {
+
+    if (this._state === S_OUT) {
+
+    	if (obj === tokenize.OPEN) {
+    		this._state = S_IN_MAIN;
+    	} else {
+    		this.emit('error', new Error('sexp stream must begin with open paren'));
+    		return;
+    	}
+
+    } else if (this._state === S_IN_MAIN) {
+
+    	if (obj === tokenize.OPEN) {
+    		this._state = S_IN_EXP;
+			this._curr = [];
+    		this._stack = [];
+    	} else if (obj === tokenize.CLOSE) {
+    		this._state = S_END;
+    	} else {
+    		this.push(obj);
+    	}
+
+    } else if (this._state === S_IN_EXP) {
+
+    	if (obj === tokenize.OPEN) {
+    	    var newSexp = [];
+    	    this._curr.push(newSexp);
+    	    this._stack.push(curr);
+    	    this._curr = newSexp;
+    	} else if (obj === tokenize.CLOSE) {
+    		if (this._stack.length === 0) {
+    			this.push(this._curr);
+    			this._state = S_IN_MAIN;
+    		} else {
+    			this._curr = this._stack.pop();
+    		}
+    	} else {
+    	    this._curr.push(obj);
+    	}
+
+    }
+
+    done();
+
+}
+
+Parser.prototype._flush = function(cb) {
+	cb(this._state === S_END ? null : new Error("unexpected EOF"));
+}
